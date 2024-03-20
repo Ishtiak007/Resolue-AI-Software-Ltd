987b67e28a682c4f1aeafce5cafaeefcd0decc72
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-03-21 02:14:55 +0600

    navbar added to the home page

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import banner from '../assets/loginBanner.jpg'
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div style={{ '--image-url': `url(${banner})` }}
+                className='hero bg-[image:var(--image-url)] relative overflow-hidden rounded-b-md bg-no-repeat text-center h-[200px] lg:h-screen bg-cover'>
+                <div className="absolute bottom-0 left-0 right-0 top-0  overflow-hidden bg-fixed bg-black bg-opacity-60">
+                    <div className="w-full h-full flex justify-center items-center">
+                        <Link to='/login'><button className='buttonProject3'>Login for Explore</button></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
